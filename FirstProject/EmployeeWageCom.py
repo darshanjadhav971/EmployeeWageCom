deeae6123befc3108e3098e5d121302272e2a9ec
darshanjadhav971 <darshanjadhav971@gmail.com>
2026-02-05 12:11:58 +0530

    [MODIFIED]:Darshan - UC-1- Completed for Employee Attendance

--- FirstProject/EmployeeWageCom.py
+++ FirstProject/EmployeeWageCom.py
@@ -1,1 +1,6 @@
-num = int(input("Welcome MY Employee Wage Computation Program :"))
+num = int(input("Employee Wage Computation Program [Apsent/Present] :"))
+attendance = 0
+if attendance == 0:
+        print("Employee is Apsent :")
+else:
+        print("Employee is Present :")
